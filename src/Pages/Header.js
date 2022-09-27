0f7a9ab8709da38a681be218ff9507dc9112ad51
SUMON <m.sumon@outlook.com>
2022-09-28 03:30:21 +0600

    nav fixed

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -8,9 +8,9 @@ const Header = () => {
     <div className=''>
       <nav className="navbar navbar-expand-lg bg-banner">
         <div className="container-fluid mt-3 ms-5">
-          <Link className="navbar-brand text-white fw-bold mb-0 lh-1" href="#">Tutor Master
+          <Link className="navbar-brand nav-logo text-white fw-bold mb-0 lh-1" href="#">Tutor Master
             <br />
-            <span className='logo-title lh-1 '>knowledge Transfer</span>
+            <span className='nav-sub-logo lh-1 '>knowledge Transfer</span>
           </Link>
           <br />
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,9 +27,9 @@ const Header = () => {
               <li className="nav-item d-margin">
                 <Link className="nav-link active text-white" aria-current="page" href="#">About</Link>
               </li>
-              <li className='d-margin me-5'>
+              <p style={{ width: '80px', height: '40px', padding: '0', marginLeft: '30px' }} className='text-center d-margin me-5 nav-link'>
                 <Link className="nav-link active text-primary bg-light" aria-current="page" href="#">Login
-                </Link></li>
+                </Link></p>
             </ul>
 
           </div>
