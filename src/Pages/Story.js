62cd422da1a0550120bd9502d872501e5e5bc184
SUMON <m.sumon@outlook.com>
2022-09-28 21:14:07 +0600

    story section added

--- src/Pages/Story.js
+++ src/Pages/Story.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import storyImage from '../assets/story.png'
+import { FaCheck } from 'react-icons/fa';
+import { Button } from 'react-bootstrap';
 
 const Story = () => {
     return (
-        <div className='m-5 d-flex justify-content-between align-items-center'>
-            <div style={{ width: '386px', fontFamily: 'poppins' }}>
-                <h3 style={{ fontSize: '35px', fontWeight: '600' }}>The Story of Tutor
+        <div style={{ margin: '120px' }} className='d-flex justify-content-between'>
+            <div style={{ width: '400px', fontFamily: 'poppins', marginTop: '50px', color: 'gray' }}>
+                <h3 style={{ fontSize: '35px', fontWeight: '600', color: 'black' }}>The Story of Tutor
                     <br />
                     Master?
                 </h3>
                 <p style={{ fontSize: '15px', fontWeight: '300', textAlign: 'justify', color: '#383838 70%', width: '386px' }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati natus quae deserunt, nobis accusantium inventore! Atque incidunt vero quos esse, quia doloremque commodi repudiandae. Tempora nobis vitae tempore nisi rerum natus.</p>
+                <p style={{ fontSize: '15px' }}><span className='text-primary me-2 fs-6'><FaCheck /></span>usp #1 showcase here with a brief as needed</p>
+                <p style={{ fontSize: '15px' }}><span className='text-primary me-2'><FaCheck /></span>usp #1 showcase here with a brief as needed</p>
+                <p style={{ fontSize: '15px', color: 'gray' }}><span className='text-primary me-2'><FaCheck /></span>usp #1 showcase here with a brief as needed</p>
+                <Button className='btn-primary rounded-0'>know more</Button>
             </div>
             <div>
                 <div style={{
@@ -20,7 +26,6 @@ const Story = () => {
                     <img style={{ width: '100%', borderRadius: '0 150px 0 0' }} src={storyImage} alt="" />
                 </div>
             </div>
-
         </div>
     );
 };
